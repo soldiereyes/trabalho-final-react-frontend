7598d259411af1aa79376110ce9b524c8f62fa01
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2024-12-30 17:14:40 -0300

    feat: implementa a pagina principal de login ( ainda não é final )

---
+++ src/componentes/Login/LoginPage/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginHeader from "../LoginHeader/LoginHeader.jsx";
+// import LoginForm from "../LoginForm/LoginForm.jsx";
+
+
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginHeader />
+            {/*<LoginForm />*/}
+        </div>
+    );
+};
+
+export default LoginPage;
