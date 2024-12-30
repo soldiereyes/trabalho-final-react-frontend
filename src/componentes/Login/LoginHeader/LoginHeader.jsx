daa08fb846917155d542a18dab1a404794e7ba02
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2024-12-30 17:15:04 -0300

    feat: cria um cabeçalho para a página a fim de teste

---
+++ src/componentes/Login/LoginHeader/LoginHeader.jsx
@@ -0,0 +1,12 @@
+import "./LoginHeader.css";
+
+const LoginHeader = () => {
+    return (
+        <header className="login-header">
+            <h1>Login</h1>
+            <p>Bem-vindo! Faça login para acessar o sistema.</p>
+        </header>
+    );
+};
+
+export default LoginHeader;
