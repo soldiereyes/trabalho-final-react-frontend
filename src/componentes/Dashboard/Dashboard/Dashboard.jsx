a8bdd85b0a24dc4e7d3e76d3169a91668218d135
Guilherme Gonzaga Soares <guilhermegonzagasoares_@hotmail.com>
2024-12-30 17:12:33 -0300

    feat: cria a dashboard para comportar os componentes relacionados a ela

---
+++ src/componentes/Dashboard/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import DashboardHeader from "../DashboardHeader/DashboardHeader.jsx";
+import DashboardTable from "../DashboardTable/DashboardTable.jsx";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <DashboardHeader />
+            <DashboardTable />
+        </div>
+    );
+};
+
+export default Dashboard;
